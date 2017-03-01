e8a60d953f2394c352edf1e154bbe4581c7f24f5
idsweb <iandstafford@googlemail.com>
2017-03-01 11:49:25 +0000

    Updated to handle duplicates

--- AddHostsFileEntry.ps1
+++ AddHostsFileEntry.ps1
@@ -1,12 +1,35 @@
-﻿# Uncomment lines with localhost on them:
+ # Simple script to add a host file entry
+# ======================================
+
 $hostsPath = "$env:windir\System32\drivers\etc\hosts"
-$hosts = get-content $hostsPath
-$addEntry = $true
-$hosts | %{
-    if($_.contains('www.foo.com')){
-        $addEntry = $false
-        break
+
+$ip = '127.0.0.1'
+$domain = 'yoursite.com'
+
+Write-Host "Adding entry for $ip`t$domain"
+
+$entryExists = $false
+
+Write-Host "Current host file entries" -ForegroundColor Yellow
+
+foreach ($line in [System.IO.File]::ReadLines($hostsPath)) {
+    # do something with $line
+    if($line.IndexOf('#') -lt 0){
+        if($line.IndexOf($domain) -gt 0){
+            $entryExists = $true
+            Write-Host $line -ForegroundColor Red
+        }
+        else{
+            Write-Host $line -ForegroundColor Yellow
+        }
     }
 }
-Add-Content $hostsPath "#`tlocalhost`twww.foo.com"
-notepad.exe $hostsPath
+
+if(-not $entryExists){
+    Write-Host "Adding entry" -ForegroundColor Yellow
+    Add-Content $hostsPath "$ip`t$domain"
+    # notepad.exe $hostsPath
+}
+else{
+    Write-Host "Entry exists already" -ForegroundColor Red
+} 
