41e1e809915675550397eabb69469f1364b02b6f
idsweb <iandstafford@googlemail.com>
2017-05-04 10:12:11 +0100

    Created generic template
    Starter template for any basic script

---
+++ generictemplate.ps1
@@ -0,0 +1,44 @@
+ # Title: Script template for callable script
+# ==========================================
+# Author: yourname
+# DateTime: createdDate
+
+<# 
+
+Description: used as a base template for my other scripts
+
+#>
+
+# Script parameters
+# -----------------
+param(
+    
+    [Parameter(Mandatory=$True,
+    Position=1,
+    HelpMessage='This is my parameter and it is compulsory'
+    )]
+    [string]$myparam
+
+    )
+
+# Functions
+# ---------
+
+# Add the PowerShell Snapin
+$snapin = Get-PSSnapin | Where-Object {$_.Name -eq 'Microsoft.SharePoint.Powershell'}
+if ($snapin -eq $null) 
+{ 
+    Add-PSSnapin "Microsoft.SharePoint.Powershell"
+}
+
+# Setup global objects
+# --------------------
+
+# Script
+# ------
+
+write-host Script started: (Get-Date -format t) -ForegroundColor Yellow
+
+write-host Script finished: (Get-Date -format t) -ForegroundColor Yellow
+
+ 
