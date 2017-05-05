bb517a49962d8157c4f6ffdd76532e247dec2af2
idsweb <iandstafford@googlemail.com>
2017-05-05 16:48:59 +0100

    Added comment and examples block

--- generictemplate.ps1
+++ generictemplate.ps1
@@ -2,12 +2,24 @@
 # ==========================================
 # Author: yourname
 # DateTime: createdDate
+ <#
 
-<# 
+.SYNOPSIS
+This is a simple Powershell script to explain how to create help
 
-Description: used as a base template for my other scripts
+.DESCRIPTION
+The script itself will only print 'Hello World'. But that's cool. It's main objective is to show off the cool help thingy anyway.
 
-#>
+.EXAMPLE
+./HelloWorld.ps1
+
+.NOTES
+Put some notes here.
+
+.LINK
+http://kevinpelgrims.wordpress.com
+
+#> 
 
 # Script parameters
 # -----------------
