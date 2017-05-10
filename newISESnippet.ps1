4cdac01272d9be5f71520de43d652947d9db6676
idsweb <iandstafford@googlemail.com>
2017-05-10 11:30:18 +0100

    Create newISESnippet.ps1

---
+++ newISESnippet.ps1
@@ -0,0 +1,10 @@
+ $M = @'
+Param
+(
+  [parameter(Mandatory=$true)]
+  [String[]]
+  $<ParameterName>
+)
+'@
+
+New-ISESnippet -Text $M -Title Mandatory -Description "Adds a mandatory function parameter." -Author "Patti Fuller, Fabrikam Corp." -Force 
